560ce80f65519a89a5736c83a3f87ba7d1ba3e4c
bdt833 <bdt833@gmail.com>
2021-01-27 10:43:34 -0600

    Included an updated version for platform_versions.py to have the actual script for inserting into the database.

--- platform_versions.py
+++ platform_versions.py
@@ -3,15 +3,17 @@ import requests, time, psycopg2
 conn = psycopg2.connect("dbname='**' user='**' host='localhost' password='**'")
 cur = conn.cursor()
 
+dict_defaults = ['companies', 'cpu', 'graphics', 'memory', 'os', 'storage', 'sound']
+
 url = "https://api.igdb.com/v4/platform_versions"
 
-lim = 50
+lim = 500
 offset = 0
 
 while offset < lim:
     payload =  f"""
-        fields id, name;
-        \r\nlimit 50;\r\noffset {offset};
+        fields id, name, companies, cpu, graphics, memory, os, storage, sound;
+        \r\nlimit 500;\r\noffset {offset};
         \r\nsort id; 
         """
     headers = {
@@ -24,10 +26,14 @@ while offset < lim:
     response = requests.request("POST", url, headers=headers, data = payload)
 
     json_data = response.json()
-    
-    cur.executemany("""INSERT INTO platform_family(platform_family_id,name) VALUES (%(id)s, %(name)s)""", json_data)
+    for data in json_data:
+        for defaults in dict_defaults:
+            data.setdefault(defaults, None)
 
-    offset += 50
+
+    cur.executemany("""INSERT INTO platform_versions(platform_version_id,name, platform_companies, cpu, graphics, memory, os, storage, sound) VALUES (%(id)s, %(name)s, %(companies)s, %(cpu)s, %(graphics)s, %(memory)s, %(os)s, %(storage)s, %(sound)s)""", json_data)
+
+    offset += 500
     time.sleep(0.250)
 
     
